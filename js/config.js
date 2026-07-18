2979e01dcaea95f2e381ce40832f14b363aca9b8
Valithron <125237424+Valithron@users.noreply.github.com>
2026-07-18 16:59:31 -0600

    Map small reaction portraits and refresh asset cache

--- js/config.js
+++ js/config.js
@@ -32,17 +32,17 @@
   });
 
   const characterPresentation = Object.freeze({
-    assetVersion: "portraits-v2",
+    assetVersion: "portraits-v5",
     genericAsset: "assets/symbols/tree-of-life.svg",
     characters: Object.freeze({
-      STR: Object.freeze({ name: "Sterling", base: "assets/symbols/sterling.svg", nice: null, big: null, accent: "#d3d8e8" }),
-      CYD: Object.freeze({ name: "Cydney", base: "assets/symbols/cydney.svg", nice: null, big: null, accent: "#86a66a" }),
-      RYN: Object.freeze({ name: "Ryan", base: "assets/symbols/ryan.svg", nice: null, big: null, accent: "#a276ff" }),
-      GAB: Object.freeze({ name: "Gabi", base: "assets/symbols/gabi.svg", nice: null, big: null, accent: "#89d2ff" }),
-      COP: Object.freeze({ name: "Cooper", base: "assets/symbols/cooper.svg", nice: null, big: null, accent: "#e0aa3e" }),
-      KEN: Object.freeze({ name: "Kenly", base: "assets/symbols/kenly.svg", nice: null, big: null, accent: "#65e6cc" }),
-      ASH: Object.freeze({ name: "Ashley", base: "assets/symbols/ashley.svg", nice: null, big: null, accent: "#ff7fba" }),
-      TOL: Object.freeze({ name: "Tree of Life", base: "assets/symbols/tree-of-life.svg", nice: null, big: null, accent: "#f1d98a" }),
+      STR: Object.freeze({ name: "Sterling", base: "assets/symbols/sterling.svg", small: "assets/symbols/sterling-small.svg", nice: "assets/symbols/sterling-nice.svg", big: "assets/symbols/sterling-big.svg", accent: "#d3d8e8" }),
+      CYD: Object.freeze({ name: "Cydney", base: "assets/symbols/cydney.svg", small: "assets/symbols/cydney-small.svg", nice: null, big: null, accent: "#86a66a" }),
+      RYN: Object.freeze({ name: "Ryan", base: "assets/symbols/ryan.svg", small: "assets/symbols/ryan-small.svg", nice: null, big: null, accent: "#a276ff" }),
+      GAB: Object.freeze({ name: "Gabi", base: "assets/symbols/gabi.svg", small: "assets/symbols/gabi-small.svg", nice: null, big: null, accent: "#89d2ff" }),
+      COP: Object.freeze({ name: "Cooper", base: "assets/symbols/cooper.svg", small: "assets/symbols/cooper-small.svg", nice: null, big: null, accent: "#e0aa3e" }),
+      KEN: Object.freeze({ name: "Kenly", base: "assets/symbols/kenly.svg", small: "assets/symbols/kenly-small.svg", nice: null, big: null, accent: "#65e6cc" }),
+      ASH: Object.freeze({ name: "Ashley", base: "assets/symbols/ashley.svg", small: "assets/symbols/ashley-small.svg", nice: null, big: null, accent: "#ff7fba" }),
+      TOL: Object.freeze({ name: "Tree of Life", base: "assets/symbols/tree-of-life.svg", small: null, nice: null, big: null, accent: "#f1d98a" }),
     }),
     allMembers: Object.freeze(["STR", "CYD", "RYN", "GAB", "COP", "KEN", "ASH"]),
     combinationMembers: Object.freeze({
